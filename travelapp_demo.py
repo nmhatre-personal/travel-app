3b7a3c3dfab65f2ca4ffb02421a7f77e8830febf
nmhatre-personal <nmhatre@gmail.com>
2025-10-18 21:14:48 -0700

    Travel App python source code

---
+++ travelapp_demo.py
@@ -0,0 +1,31 @@
+import os
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+import streamlit as st
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI(model="gpt-4",api_key=OPENAI_API_KEY)
+
+prompt_template = PromptTemplate(
+    input_variables = ["city","month","language","budget"],
+    template = """
+    Welcome to the {city} travel guide!
+    If you are visiting in {month}, then here is what you can do:
+    1. Must visit attractions.
+    2. Local cuisine you must try.
+    3. Useful phrases in {language}.
+    4. Tips for travelling on a {budget} budget.
+    Enjoy your trip!
+    """
+)
+
+st.title("Travel App")
+
+city = st.text_input("Enter the city name")
+month = st.text_input("Enter the month name")
+language = st.text_input("Enter the language name")
+budget = st.number_input("Enter the budget")
+if city:
+    response = llm.invoke(prompt_template.format(city=city,month=month,language=language,budget=budget))
+    st.write(response.content)
